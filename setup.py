7351d141284025df52dbf480ecbd5f4e6d253f61
roni <ronigoldsmid@gmail.com>
2020-07-13 18:24:26 +0300

    -v0.01

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'modelboost',   
+  packages = ['modelboost'],  
+  version = '0.0.1',      
+  license='MIT',      
+  description = 'A package that makes the process of finding the model and optimizing the model automated and simple',
+  author = 'Roni Gold',                  
+  author_email = 'ronigoldsmid@gmail.com', 
+  url = 'https://github.com/ronigold',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['machine learning', 'deep learning', 'model', 'optimizing'],  
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
